29b149b066ea64310be018f70f7014a117e63146
strixpanahu <schimpfzach@gmail.com>
2024-04-29 21:50:18 -0500

    Complete steps A, C, D, H, I, J

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 Select any one of these labs (1, 2, 3, 9, and 12) and improve its code using the concepts we have covered in this class. Your project should have the items/features below:
 
-	- [ ] A. Code is organized into modules where applicable (lab 11 is a good example of code separation).
+	- [x] A. Code is organized into modules where applicable (lab 11 is a good example of code separation).
 
 	- [ ] B. Programs that use/retrieve/store data use files/databases/API.
 
-	- [ ] C. Data validation and exception handling is present.
+	- [x] C. Data validation and exception handling is present.
 
-	- [ ] D. Algorithms/data structures/data types used appropriately.
+	- [x] D. Algorithms/data structures/data types used appropriately.
 
 	- [ ] E. OOP best practices implemented (Separating code to classes, data hiding, inheritance - lab 10 and lab 12 are great examples).
 
@@ -16,10 +16,10 @@ Select any one of these labs (1, 2, 3, 9, and 12) and improve its code using the
 
 	- [ ] G. Proper documentation.
 
-	- [ ] H. Using descriptive variable names, class names, and file names.
+	- [x] H. Using descriptive variable names, class names, and file names.
 
-	- [ ] I. Docstrings for functions.
+	- [x] I. Docstrings for functions.
 
-	- [ ] J. Type hinting.
+	- [x] J. Type hinting.
 
 	- [ ] K. Public GitHub link submitted with all project files placed/merged into the main/master branch. The project files could range from python files, images, documents, images, sound files, text files, binary files, executable files, etc. (generally anything that is needed for the application to work).
--- ZacSchimpf1.py
+++ ZacSchimpf1.py
@@ -7,127 +7,128 @@
 """
 
 
-def main():
-    """
-    Primary loop responsible for taking, tracking & printing votes
-    :return: None
-    """
-    candidates = ["John", "Jane"]
-    total_votes = []
+class Voter:
+    def __init__(self) -> None:
+        """
+        Primary loop responsible for taking, tracking & printing votes
+        :return: None
+        """
+        candidates = ["John", "Jane"]
+        total_votes = []
 
-    option = None
-    while option != 'x':
-        option = vote_menu()
+        option = None
+        while option != 'x':
+            option = self.vote_menu()
 
-        if option != 'x':
-            # Shift value to account for list[] indexing
-            current_vote = candidate_menu() - 1
+            if option != 'x':
+                # Shift value to account for list[] indexing
+                current_vote = self.candidate_menu() - 1
 
-            total_votes.append(candidates[current_vote])
-            print("Voted " + candidates[current_vote])
+                total_votes.append(candidates[current_vote])
+                print("Voted " + candidates[current_vote])
 
-    print(
-        breaker() + '\n' +
-        "John – " + str(total_votes.count("John")) +
-        ", Jane – " + str(total_votes.count("Jane")) +
-        ", Total – " + str(len(total_votes)) + '\n' +
-        breaker()
-    )
-
-
-def vote_menu():
-    """
-    Primary menu containing navigation options
-    :return: Menu option, single-char string; e.g. 'v', 'x'
-    """
-
-    print(
-        breaker() + '\n' +
-        "VOTE MENU" + '\n' +
-        breaker() + '\n' + 
-        "v: Vote" + '\n' +
-        "x: Exit"
-    )
-    option = input("Option: ")
-    return menu_validation(option)
+        print(
+            self.breaker() + '\n' +
+            "John – " + str(total_votes.count("John")) +
+            ", Jane – " + str(total_votes.count("Jane")) +
+            ", Total – " + str(len(total_votes)) + '\n' +
+            self.breaker()
+        )
 
 
-def menu_validation(option):
-    """
-    Clean & validate user's navigation option
-    :param option: Raw user input, single-char string; e.g. 'v', 'x'
-    :return: Validated option, single-char string; e.g. 'v', 'x'
-    """
+    def vote_menu(self) -> str:
+        """
+        Primary menu containing navigation options
+        :return: Menu option, single-char string; e.g. 'v', 'x'
+        """
 
-    option = option.strip().casefold()
-
-    if option != 'v' and option != 'x':
-        option = vote_menu_redo()
-
-    return option
-
-
-def vote_menu_redo():
-    """
-    Primary menu redo prompt
-    :return: raw user input
-    """
-
-    option = menu_validation(input("Invalid (v/x): "))
-    return option
+        print(
+            self.breaker() + '\n' +
+            "VOTE MENU" + '\n' +
+            self.breaker() + '\n' +
+            "v: Vote" + '\n' +
+            "x: Exit"
+        )
+        option = input("Option: ")
+        return self.menu_validation(option)
 
 
-def candidate_menu():
-    """
-    Secondary menu containing voter options
-    :return: Voter choice string; e.g. "John", "Jane"
-    """
+    def menu_validation(self, option) -> str:
+        """
+        Clean & validate user's navigation option
+        :param option: Raw user input, single-char string; e.g. 'v', 'x'
+        :return: Validated option, single-char string; e.g. 'v', 'x'
+        """
 
-    print(
-        breaker() + '\n' +
-        "CANDIDATE MENU" + '\n' +
-        breaker() + '\n' +
-        "1: John" + '\n' +
-        "2: Jane"
-    )
-    option = input("Option: ")
-    return candidate_validation(option)
+        option = option.strip().casefold()
+
+        if option != 'v' and option != 'x':
+            option = self.vote_menu_redo()
+
+        return option
 
 
-def candidate_validation(option):
-    """
-    Clean & validate user's navigation option
-    :param option: Menu option, single-char string; e.g. '1', '2'
-    :return: Voter choice int; e.g. 1, 2
-    """
+    def vote_menu_redo(self) -> str:
+        """
+        Primary menu redo prompt
+        :return: raw user input
+        """
 
-    option = option.strip().casefold()
-
-    if option != '1' and option != '2':
-        option = candidate_menu_redo()
-
-    return int(option)
+        option = self.menu_validation(input("Invalid (v/x): "))
+        return option
 
 
-def candidate_menu_redo():
-    """
-    Secondary menu redo prompt
-    :return: raw user input
-    """
+    def candidate_menu(self) -> str:
+        """
+        Secondary menu containing voter options
+        :return: Voter choice string; e.g. "John", "Jane"
+        """
 
-    option = candidate_validation(input("Invalid (1/2): "))
-    return option
-
-
-def breaker():
-    """
-    :return: Standardised size line break
-    """
-
-    breaker_len = 29
-    breaker_char = '-'
-    breaker_str = breaker_char * breaker_len
-    return breaker_str
+        print(
+            self.breaker() + '\n' +
+            "CANDIDATE MENU" + '\n' +
+            self.breaker() + '\n' +
+            "1: John" + '\n' +
+            "2: Jane"
+        )
+        option = input("Option: ")
+        return self.candidate_validation(option)
 
 
-main()
+    def candidate_validation(self, option) -> int:
+        """
+        Clean & validate user's navigation option
+        :param option: Menu option, single-char string; e.g. '1', '2'
+        :return: Voter choice int; e.g. 1, 2
+        """
+
+        option = option.strip().casefold()
+
+        if option != '1' and option != '2':
+            option = self.candidate_menu_redo()
+
+        return int(option)
+
+
+    def candidate_menu_redo(self) -> str:
+        """
+        Secondary menu redo prompt
+        :return: raw user input
+        """
+
+        option = self.candidate_validation(input("Invalid (1/2): "))
+        return option
+
+
+    def breaker(self) -> str:
+        """
+        :return: Standardised size line break
+        """
+
+        breaker_len = 29
+        breaker_char = '-'
+        breaker_str = breaker_char * breaker_len
+        return breaker_str
+
+
+test = Voter()
